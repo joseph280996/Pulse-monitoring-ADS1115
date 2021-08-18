681d20fc94b3397ae3aa92e047ad03e488b78668
Tung Pham <joseph.pham280996@gmail.com>
2021-08-18 00:34:28 -0400

    Clean up, create data extraction and write to file functionality for export

---
+++ src/server/routes/recordsHandlers.ts
@@ -0,0 +1,47 @@
+import fs from 'fs'
+import { RequestHandler } from 'express'
+import { pick } from 'lodash'
+import Patient from '../model/Patient'
+import Record from '../model/Record'
+
+const recordData: RequestHandler = async (req, res) => {
+  try {
+    let foundPatient = await Patient.findPatientByName(req.body.patientName)
+    if (!foundPatient) {
+      foundPatient = new Patient({ name: req.body.patientName })
+      await foundPatient.save()
+    }
+    const newRecord = new Record({
+      patientID: foundPatient.id as number,
+      ...pick(req.body, ['id', 'pulseTypeID', 'handPositionID', 'data']),
+    })
+    const savedRecord = await newRecord.save()
+    res.status(200).send(savedRecord)
+  } catch (err) {
+    console.error(err)
+    console.log(err.stack)
+    res.status(500).send('Internal Error')
+  }
+}
+
+export default recordData
+
+export const exportData: RequestHandler = async (req, res) => {
+  try {
+    if (req.body.export) {
+      const { startDate, endDate } = req.body
+
+      const records = await Record.getByDateRange({ startDate, endDate })
+      Promise.all(
+        records.map(async (record) => Patient.getById(record.patientID)),
+      )
+      const stringifiedRecords = JSON.stringify(records)
+      fs.writeFileSync('./data.json', stringifiedRecords)
+    }
+    res.status(200).send({ status: 200 })
+  } catch (err) {
+    console.error(err)
+    console.log(err.stack)
+    res.status(500).send('Internal Error')
+  }
+}
