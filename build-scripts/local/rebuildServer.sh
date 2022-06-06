2364b9b88f30cf8324ccf0871b0f927090b8f3c4
Tung Pham <joseph.pham280996@gmail.com>
2022-06-06 00:01:05 -0400

    Remove build scripts since migrating over to integration repo

--- build-scripts/local/rebuildServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose down pulse-monitoring-server
-docker-compose build pulse-monitoring-server
-docker-compose up pulse-monitoring-server -d
--- build-scripts/local/reset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker-compose down --rmi all
-docker-compose up -d
