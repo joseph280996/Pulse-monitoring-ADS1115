7b1317b378a411e406d70b3e9b4faeda5dc8b443
Tung Pham <joseph.pham280996@gmail.com>
2022-06-14 00:01:03 -0400

    Move load all out of pulsetype

--- src/server/routes/pulseTypesHandlers.ts
+++ src/server/routes/pulseTypesHandlers.ts
@@ -1,8 +1,18 @@
 import { RequestHandler } from 'express'
+import db from '../db'
 import PulseType from '../models/PulseTypes'
+import { PulseTypeDataType } from '../models/PulseTypes.types'
+import * as PulseTypeSqls from '../sqls/pulseTypeSqls'
 
 const getPulseTypes: RequestHandler = async (_req, res) => {
-  const pulseTypes = await PulseType.loadAll()
+  const pulseTypes = await db
+    .query<PulseTypeDataType[], void>(PulseTypeSqls.GET_ALL)
+    .then((result: PulseTypeDataType[]) => {
+      if (result?.length > 0) {
+        return result.map((row: PulseTypeDataType) => new PulseType(row))
+      }
+      return []
+    })
   res.status(200).send(pulseTypes)
 }
 
