54ac6673ea2bbfd77c855d094277b5898f4d271d
Tung Pham <joseph.pham280996@gmail.com>
2022-06-13 23:57:30 -0400

    Restructure db

--- sql/HandPosition.sql
+++ sql/HandPosition.sql
@@ -2,5 +2,10 @@ CREATE TABLE HandPosition (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255)
 );
-INSERT INTO HandPosition(name) 
-VALUES ('left upper'), ('left middle'), ('left bottom'), ('right upper'), ('right middle'), ('right bottom');
+INSERT INTO HandPosition(name)
+VALUES ('left upper'),
+  ('left middle'),
+  ('left bottom'),
+  ('right upper'),
+  ('right middle'),
+  ('right bottom');
