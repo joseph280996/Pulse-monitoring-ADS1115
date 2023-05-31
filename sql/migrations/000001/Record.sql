0533a3d09fc6ef21d37eca1b46cb09884f755525
Tung Pham <joseph.pham280996@gmail.com>
2023-05-30 22:10:41 -0400

    Update Migration

--- sql/migrations/000001/Record.sql
+++ sql/migrations/000001/Record.sql
@@ -2,12 +2,17 @@ ALTER TABLE Record
 ADD COLUMN RecordSessionId INT UNSIGNED,
 ADD FOREIGN KEY(RecordSessionId) REFERENCES RecordSession(Id);
 
+ALTER TABLE Record
+CHANGE COLUMN ID Id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ;
+
+SET SQL_SAFE_UPDATES=0;
 UPDATE Record r
 INNER JOIN RecordSession rs ON r.DiagnosisId = rs.DiagnosisId AND r.RecordTypeId = rs.RecordTypeId
 SET r.RecordSessionId = rs.Id
-WHERE Id > 0;
+WHERE r.Id > 0;
+SET SQL_SAFE_UPDATES=1;
 
 ALTER TABLE Record
-DROP FOREIGN KEY Record_ibfk_6,
+DROP FOREIGN KEY Record_ibfk_4,
 DROP COLUMN DiagnosisId,
 DROP COLUMN RecordTypeId;
