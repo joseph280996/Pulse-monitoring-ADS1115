e9461c90b022688c50dd44c6e85e5647bd4868ca
Tung Pham <joseph.pham280996@gmail.com>
2023-08-27 12:20:16 -0400

    Remove interface from models

--- src/server/domain/interfaces/IPatient.ts
+++
@@ -1,7 +0,0 @@
-interface IPatient {
-  id?: number
-  userId?: number
-  firstName?: string
-  lastName?: string
-}
-export default IPatient
--- src/server/domain/interfaces/IUser.ts
+++
@@ -1,6 +0,0 @@
-interface IUser {
-  id?: number
-  firstName: string
-  lastName?: string
-}
-export default IUser
